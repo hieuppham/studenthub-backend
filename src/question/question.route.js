8ad54841c175e382765dcb7e538a8a55f900c108
phamt <phamtrunghieu6d@gmail.com>
2022-04-01 10:25:28 +0700

    add getAllTags

--- src/question/question.route.js
+++ src/question/question.route.js
@@ -1,18 +1,30 @@
-const router = require('express').Router()
-const { addQuestion, findQuestions, findQuestionById, updateQuestionById, deleteQuesionById } = require("./question.controller")
-const { updateQuestionScoreById } = require('./question-voter.controller')
-const { addQuestionComment, updateQuestionCommentById, deleteQuestionCommentById } = require("./question-comment.controller")
-const { authorize } = require("../middlewares/firebase.middleware")
+const router = require("express").Router();
+const {
+  addQuestion,
+  findQuestions,
+  findQuestionById,
+  updateQuestionById,
+  deleteQuesionById,
+} = require("./question.controller");
+const { updateQuestionScoreById } = require("./question-voter.controller");
+const {
+  addQuestionComment,
+  updateQuestionCommentById,
+  deleteQuestionCommentById,
+} = require("./question-comment.controller");
+const { authorize } = require("../middlewares/firebase.middleware");
+const { getAllTags } = require("./tag.controller");
 
 function getUid(req, res, next) {
-    res.locals.uid = req.body.userId;
-    next();
+  res.locals.uid = req.body.userId;
+  next();
 }
 
 const authQueue = [getUid, authorize];
 
 router.get("/", findQuestions);
 router.get("/:id", findQuestionById);
+router.get("/property/tags", getAllTags);
 
 router.post("/", authQueue, addQuestion);
 router.put("/:id", authQueue, updateQuestionById);
